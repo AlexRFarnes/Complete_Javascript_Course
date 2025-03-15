cf9ffd44789ac78091839e294ba1b2133aafda8f
Alexander R. Farnes <alexrfarnes@gmail.com>
2025-03-15 10:56:24 +0800

    Add functionality to close modal with escape key

--- modal/script.js
+++ modal/script.js
@@ -20,3 +20,10 @@ for (let i = 0; i < btnsOpenModal.length; i++) {
 btnCloseModal.addEventListener('click', closeModal);
 
 overlay.addEventListener('click', closeModal);
+
+document.addEventListener('keydown', e => {
+  // Check if modal is showing
+  if (e.key === 'Escape' && !modal.classList.contains('hidden')) {
+    closeModal();
+  }
+});
